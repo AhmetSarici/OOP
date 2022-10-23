e05d05a1349aeabc81f72888d53c559f1c7072cc
AhmetSarici <ahmetsarici1@gmail.com>
2022-10-24 00:01:46 +0300

    Uygulama -8

---
+++ Deconstruct/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Sınıfın nesnesi üzerinden geriye hızlıca tupple bir değer döndüren yapı
+        //Özel sınıf memberlarının ortak özelliği sınıf isimlerini taşımakta idi
+        //Deconstruct deconstruct isminde olmalıdır.***
+        #region Deconstruct
+        Person p1 = new()
+        {
+            Name = "Veli",
+            Age = 27
+        };
+        var (x, y) = p1;
+        System.Console.WriteLine($"{x} kişisinin yaşı {y}' dir.");
+
+        #endregion
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public void Deconstruct(out string ad, out int yas)
+    {
+        ad = Name;
+        yas = Age;
+    }
+}
